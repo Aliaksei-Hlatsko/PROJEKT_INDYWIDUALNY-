2bdb56bec8eb7a33c0a22e5ed26678dc83c4d21b
Alen-Berg <65113327+Alen-Berg@users.noreply.github.com>
2021-11-29 14:01:03 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+menuToggle.onclick  =  function(){
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
